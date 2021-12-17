5ede825bfe7bb36fadbbe5a7a09c464886d0edc5
David Sousa <62266135+ctrl-afk@users.noreply.github.com>
2021-12-17 13:05:01 -0300

    rebuild

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,7 @@ import { FindComponent } from './components/find/find.component';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'home', pathMatch: 'full'},
-  {path: 'home', component: HomeComponent},
+  {path: '', component: HomeComponent},
   {path: 'details/:id', component: DetailsComponent},
   {path: 'find', component: FindComponent}
 ];
